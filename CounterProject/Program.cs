0c1f25eadf5461f46d9460e6f5f571da52bc3412
Greg Doud <gpdoud@gmail.com>
2018-08-18 08:54:05 -0400

    Updated Program.cs

--- CounterProject/Program.cs
+++ CounterProject/Program.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace CounterProject {
 	class Program {
+		bool isAvailable = true;
+
+		void Run() {
+			for(int idx = 0, nbr = 1; idx < 10; idx++, nbr++) {
+				Console.WriteLine($"IDX is {idx}; nbr is {nbr}");
+			}
+
+			for(bool continueLooping = true; continueLooping; ) {
+				continueLooping = false;
+			}
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
